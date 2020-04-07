0a9361f177c12b10087529aae2025447de7371ed
Philip Wright <wrightphilip4@gmail.com>
2020-04-07 20:29:59 +0100

    Remove now unnecessary logging config

--- src/chess/Start.java
+++ src/chess/Start.java
@@ -1,12 +1,10 @@
 package chess;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.BasicConfigurator;
 
 public class Start {
     static Logger logger = Logger.getLogger(Start.class);
     public static void main(String[] args){
-        BasicConfigurator.configure();
         logger.info("Hello World!");
     }
 }
