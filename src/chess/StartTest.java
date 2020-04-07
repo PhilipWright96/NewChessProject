214cb24e25a25e2027d665a8bd63b975f0691135
Philip Wright <wrightphilip4@gmail.com>
2020-04-07 20:30:15 +0100

    Add basic test class

---
+++ src/chess/StartTest.java
@@ -0,0 +1,19 @@
+package chess;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StartTest {
+
+    @Test
+    public void testNothing(){
+        assertEquals("Correct", 1, 1);
+    }
+
+    // @Test
+    // public void testWillAlwaysFail(){
+    //     fail("Error message");
+    // }
+
+}
