01e57fdb13ceec4ece94ffedda61469d78fadd70
Philip Wright <wrightphilip4@gmail.com>
2020-04-25 11:52:20 +0100

    Rename class

--- src/chess/GameManager.java
+++ src/chess/GameManager.java
@@ -2,8 +2,8 @@ package chess;
 
 import org.apache.log4j.Logger;
 
-public class Start {
-    static Logger logger = Logger.getLogger(Start.class);
+public class GameManager {
+    static Logger logger = Logger.getLogger(GameManager.class);
     public static void main(String[] args){
         Board board = new Board();
         board.showBoard();
