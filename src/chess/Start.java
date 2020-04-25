32168f5c7206d896143a00baa5374c45e37673d9
Philip Wright <wrightphilip4@gmail.com>
2020-04-25 11:29:14 +0100

    Use board object

--- src/chess/Start.java
+++ src/chess/Start.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 public class Start {
     static Logger logger = Logger.getLogger(Start.class);
     public static void main(String[] args){
-        logger.info("Hello World!");
+        Board board = new Board();
+        board.showBoard();
     }
 }
