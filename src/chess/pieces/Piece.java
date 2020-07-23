6fa47f49b1848cccd1e75051d5853bf3a0c95257
Philip Wright <wrightphilip4@gmail.com>
2020-07-23 20:32:26 +0100

    Move types and team logic to abstract class

--- src/chess/pieces/Piece.java
+++ src/chess/pieces/Piece.java
@@ -6,7 +6,20 @@ public abstract class Piece {
         GOLD
     }
 
+    public enum Types {
+        PAWN,
+        ROOK,
+        KNIGHT,
+        BISHOP,
+        QUEEN,
+        KING
+    }
+
     public Piece.Teams team;
+
+    public void setTeam(Piece.Teams team){
+        this.team = team;
+    }
     
     public Piece.Teams getTeam(){
         if (this.team == Piece.Teams.SILVER){
