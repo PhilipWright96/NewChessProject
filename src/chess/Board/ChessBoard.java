9f2ccee0ac3a7dbbac432d7d07e05f38ba32ab9d
Philip Wright <wrightphilip4@gmail.com>
2020-07-27 20:51:34 +0100

    Fix chess board

--- src/chess/Board/ChessBoard.java
+++
@@ -1,2 +0,0 @@
-            chessBoardPieces[k][1] = PieceFactory.constructPiece(Piece.Types.PAWN, Piece.Teams.GOLD);
-            chessBoardPieces[k][chessBoardPieces.length - 2] = PieceFactory.constructPiece(Piece.Types.PAWN, Piece.Teams.SILVER);
