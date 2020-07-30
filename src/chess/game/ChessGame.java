42f4fff7c5bd7048090714149f5fa6402eaa872d
Philip Wright <wrightphilip4@gmail.com>
2020-07-30 20:42:04 +0100

    Update ChessGame constructor for Players

--- src/chess/game/ChessGame.java
+++ src/chess/game/ChessGame.java
@@ -1,13 +1,29 @@
 package chess.game;
 
 import chess.board.ChessBoard;
+import chess.player.Player;
+import chess.util.Teams;
 
 public class ChessGame {
     private ChessBoard board;
+
+    private Player playerSilver;
+    private Player playerGold;
     
     private boolean isRunning = false;
     private boolean isFinished = false; 
-    private int turnsTaken; 
+    private int turnsTaken;
+
+    public ChessGame(Player playerOne, Player playerTwo){
+        if (playerOne.getTeam() == Teams.SILVER){
+            this.playerSilver = playerOne;
+            this.playerGold = playerTwo;
+        }
+        else {
+            this.playerSilver = playerTwo;
+            this.playerGold = playerOne;
+        }
+    }
 
     public void start() {
         this.board = new ChessBoard();
