b563429942886e1d4e041ffd4a8cc9396497cb0d
Philip Wright <wrightphilip4@gmail.com>
2020-07-30 20:41:42 +0100

    Have game manager construct Players for Game

--- src/chess/GameManager.java
+++ src/chess/GameManager.java
@@ -3,11 +3,20 @@ package chess;
 import org.apache.log4j.Logger;
 
 import chess.game.ChessGame;
+import chess.player.Player;
+import chess.util.Teams;
 
 public class GameManager {
     static Logger logger = Logger.getLogger(GameManager.class);
     public static void main(String[] args){
-        ChessGame game = new ChessGame();
+        ChessGame game = createChessGame();
         game.start();
+    }
+
+    private static ChessGame createChessGame(){
+        Player playerSilver = new Player("Player 1", Teams.SILVER);
+        Player playerGold = new Player("Player 1", Teams.GOLD);
+
+        return new ChessGame(playerSilver, playerGold);
     }
 }
