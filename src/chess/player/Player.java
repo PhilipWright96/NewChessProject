dee8e4631dff747bb096a496dca437efaf34a0e0
Philip Wright <wrightphilip4@gmail.com>
2020-07-30 20:28:53 +0100

    Add basic Player class

---
+++ src/chess/player/Player.java
@@ -0,0 +1,14 @@
+package chess.player;
+
+import chess.util.Teams;
+
+public class Player {
+    private String username;
+    private Teams team;
+
+    public Player (String username, Teams team){
+        this.username = username;
+        this.team = team;
+    }
+    
+}
