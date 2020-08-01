a4663dc208dac1a95afcce7ff9d9c5a91877e269
Philip Wright <wrightphilip4@gmail.com>
2020-08-01 17:46:09 +0100

    Use new observer class

--- src/chess/GameManager.java
+++ src/chess/GameManager.java
@@ -3,20 +3,33 @@ package chess;
 import org.apache.log4j.Logger;
 
 import chess.game.ChessGame;
+import chess.game.GameObserver;
 import chess.player.Player;
 import chess.util.Teams;
 
 public class GameManager {
     static Logger logger = Logger.getLogger(GameManager.class);
-    public static void main(String[] args){
+    static GameObserver observer;
+    public static void main(String[] args) throws InterruptedException {
         ChessGame game = createChessGame();
         game.start();
+        if (observer.hasFinished()){
+            System.out.println("Game finished");
+        }
     }
 
     private static ChessGame createChessGame(){
         Player playerSilver = new Player("Player 1", Teams.SILVER);
         Player playerGold = new Player("Player 2", Teams.GOLD);
 
-        return new ChessGame(playerSilver, playerGold);
+        ChessGame game = new ChessGame(playerSilver, playerGold);
+
+        setObserver(game);
+        return game;
+    }
+
+    private static void setObserver(ChessGame game) {
+        observer = new GameObserver(game);
+        game.attach(observer);
     }
 }
