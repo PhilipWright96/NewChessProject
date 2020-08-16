5693d4c800ec68c66dc8350d6eba1c15564e8353
Philip Wright <wrightphilip4@gmail.com>
2020-08-16 15:42:52 +0100

    Extract regex to variable

--- src/chess/game/InputChecker.java
+++ src/chess/game/InputChecker.java
@@ -3,12 +3,14 @@ package chess.game;
 import java.util.regex.*;
 
 public class InputChecker {
+    private static final String VALID_CHESS_MOVE = "[a-e][1-8]\\-[a-e][1-8]";
+
     public static boolean checkPlayerInput(String input){
         return correctInputSyntax(input);
     }
 
     private static boolean correctInputSyntax(String input){
-        boolean result = Pattern.matches("[a-e][1-8]\\-[a-e][1-8]", input);
+        boolean result = Pattern.matches(VALID_CHESS_MOVE, input);
         if (result == false){
             System.out.println("Input : " + input + " is a invalid move. Correct input format is a1-a3. Please try again");
         }
