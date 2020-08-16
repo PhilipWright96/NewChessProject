6710e958e86ea99133d7492780043a66c53f9896
Philip Wright <wrightphilip4@gmail.com>
2020-08-16 16:42:15 +0100

    Add extra check for user input using board

--- src/chess/game/InputChecker.java
+++ src/chess/game/InputChecker.java
@@ -1,12 +1,15 @@
 package chess.game;
 
+import java.util.Arrays;
 import java.util.regex.*;
+
+import chess.pieces.Piece;
 
 public class InputChecker {
     private static final String VALID_CHESS_MOVE = "[a-e][1-8]\\-[a-e][1-8]";
 
-    public static boolean checkPlayerInput(String input){
-        return correctInputSyntax(input);
+    public static boolean checkPlayerInput(String input, Piece[][] board){
+        return correctInputSyntax(input) && correctInputLogic(input, board);
     }
 
     private static boolean correctInputSyntax(String input){
@@ -15,5 +18,20 @@ public class InputChecker {
             System.out.println("Input : " + input + " is a invalid move. Correct input format is a1-a3. Please try again");
         }
         return result;
+    }
+
+    private static boolean correctInputLogic(String input, Piece[][] board){
+
+        char[] inputArray = input.toCharArray();
+        char[] moveFrom = Arrays.copyOfRange(inputArray, 0, 2);
+        
+        int moveFromColumn = moveFrom[0] - 97;
+        int moveFromRow = Character.getNumericValue(moveFrom[1] - 1);
+
+        if (board[moveFromColumn][moveFromRow] == null){
+            System.out.println("No piece found to move");
+            return false;
+        };
+        return true;
     }
 }
