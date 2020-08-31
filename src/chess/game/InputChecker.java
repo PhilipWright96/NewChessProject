e410113d0221429e7cf322021558856016cffec0
Philip Wright <wrightphilip4@gmail.com>
2020-08-31 13:20:46 +0100

    Use new move class in input checks

--- src/chess/game/InputChecker.java
+++ src/chess/game/InputChecker.java
@@ -3,13 +3,14 @@ package chess.game;
 import java.util.Arrays;
 import java.util.regex.*;
 
+import chess.board.ChessBoard;
 import chess.pieces.Piece;
 import chess.player.Player;
 
 public class InputChecker {
     private static final String VALID_CHESS_MOVE = "[a-h][1-8]\\-[a-h][1-8]";
 
-    public static boolean checkPlayerInput(String input, Player player, Piece[][] board){
+    public static boolean checkPlayerInput(String input, Player player, ChessBoard board){
         return correctInputSyntax(input) && correctInputLogic(input, player, board);
     }
 
@@ -21,15 +22,8 @@ public class InputChecker {
         return result;
     }
 
-    private static boolean correctInputLogic(String input, Player player, Piece[][] board){
-
-        char[] inputArray = input.toCharArray();
-        char[] moveFrom = Arrays.copyOfRange(inputArray, 0, 2);
-        
-        int moveFromColumn = moveFrom[0] - 97;
-        int moveFromRow = Character.getNumericValue(moveFrom[1] - 1);
-
-        Piece pieceBeingMoved = board[moveFromColumn][moveFromRow];
+    private static boolean correctInputLogic(String input, Player player, ChessBoard board){
+        Piece pieceBeingMoved = board.getPieceBeingMovedFromBoard(new ChessMove(input));
 
         if (pieceBeingMoved == null){
             System.out.println("No piece found to move");
