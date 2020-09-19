86c4b42249fcff832e2dc7e8c41bb2f92567d5b2
Philip Wright <wrightphilip4@gmail.com>
2020-09-19 20:00:26 +0100

    Add rook piece move logic

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -5,6 +5,24 @@ import chess.game.ChessMove;
 public class Rook extends Piece {
 
     public boolean moveValid(ChessMove move){
+        if (columnMoved(move) && rowMoved(move)){
+            System.out.println("Error: Incorrect move logic for rook");
+            return false;
+        }
         return true;
+    }
+
+    private boolean columnMoved(ChessMove move){
+        if (move.getMoveFromColumn() != move.getMoveToColumn()){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean rowMoved(ChessMove move){
+        if (move.getMoveFromRow() != move.getMoveToRow()){
+            return true;
+        }
+        return false;
     }
 }
