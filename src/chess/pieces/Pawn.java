e2e076ba78d1080d16086ef06503c626187207bc
Philip Wright <wrightphilip4@gmail.com>
2020-09-19 11:38:44 +0100

    Add logic to check pawn only moves up one space

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -1,10 +1,36 @@
 package chess.pieces;
 
 import chess.game.ChessMove;
+import chess.util.Teams;
 
 public class Pawn extends Piece{
 
     public boolean moveValid(ChessMove move){
-        return true;
+        if (checkCorrectColumn(move) && checkCorrectRow(move)){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkCorrectColumn(ChessMove move){
+        if (move.getMoveFromColumn() == move.getMoveToColumn()){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkCorrectRow(ChessMove move){
+        if (getTeam() == Teams.SILVER){
+            if (move.getMoveFromRow() - 1 == move.getMoveToRow()){
+                return true;
+            }
+            return false;
+        }
+        else {
+            if (move.getMoveFromRow() + 1 == move.getMoveToRow()){
+                return true;
+            }
+            return false;
+        }
     }
 }
