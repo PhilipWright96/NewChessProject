ebe3f00ef5dd7ccba5b16b2223ab3a181c98d7b0
Philip Wright <wrightphilip4@gmail.com>
2020-09-19 20:32:38 +0100

    Add king move logic

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -5,6 +5,35 @@ import chess.game.ChessMove;
 public class King extends Piece {
 
     public boolean moveValid(ChessMove move){
-        return true;
+        if (rowChangeValid(move) && columnChangeValid(move)){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean rowChangeValid(ChessMove move){
+        if (move.getMoveFromRow() == move.getMoveToRow()){
+            return true;
+        }
+        if (move.getMoveFromRow() + 1 == move.getMoveToRow()){
+            return true;
+        }
+        if (move.getMoveFromRow() - 1 == move.getMoveToRow()){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean columnChangeValid(ChessMove move){
+        if (move.getMoveFromColumn() == move.getMoveToColumn()){
+            return true;
+        }
+        if (move.getMoveFromColumn() + 1 == move.getMoveToColumn()){
+            return true;
+        }
+        if (move.getMoveFromColumn() - 1 == move.getMoveToColumn()){
+            return true;
+        }
+        return false;
     }
 }
