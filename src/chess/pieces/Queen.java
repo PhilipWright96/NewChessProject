64d846e629d39b4bd64f395edd3136710d6c8d96
Philip Wright <wrightphilip4@gmail.com>
2020-09-20 14:07:21 +0100

    Add queen move logic

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -5,6 +5,20 @@ import chess.game.ChessMove;
 public class Queen extends Piece {
 
     public boolean moveValid(ChessMove move){
+        if (getRowChangeNum(move) == getColumnChangeNum(move)){
+            return true;
+        }
+        else if (getRowChangeNum(move) > 0 && getColumnChangeNum(move) > 0){
+            return false;
+        }
         return true;
+    }
+
+    private int getRowChangeNum(ChessMove move){
+        return Math.abs(move.getMoveFromRow() - move.getMoveToRow());
+    }
+
+    private int getColumnChangeNum(ChessMove move){
+        return Math.abs(move.getMoveFromColumn() - move.getMoveToColumn());
     }
 }
