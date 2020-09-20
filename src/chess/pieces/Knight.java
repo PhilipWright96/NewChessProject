83d81fb91abd726d18683fa49872ad436c7fc1c2
Philip Wright <wrightphilip4@gmail.com>
2020-09-20 14:22:22 +0100

    Add knight move logic

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -5,6 +5,20 @@ import chess.game.ChessMove;
 public class Knight extends Piece {
 
     public boolean moveValid(ChessMove move){
-        return true;
+        if (getRowChangeNum(move) == 1 && getColumnChangeNum(move) == 2){
+            return true;
+        }
+        else if (getRowChangeNum(move) == 2 && getColumnChangeNum(move) == 1){
+            return true;
+        }
+        return false;
+    }
+
+    private int getRowChangeNum(ChessMove move){
+        return Math.abs(move.getMoveFromRow() - move.getMoveToRow());
+    }
+
+    private int getColumnChangeNum(ChessMove move){
+        return Math.abs(move.getMoveFromColumn() - move.getMoveToColumn());
     }
 }
