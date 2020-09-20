5a3b283b9238c488f57837951201600474d2bc75
Philip Wright <wrightphilip4@gmail.com>
2020-09-20 13:50:34 +0100

    Add bishop move logic

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -5,6 +5,18 @@ import chess.game.ChessMove;
 public class Bishop extends Piece {
 
     public boolean moveValid(ChessMove move){
-        return true;
+        if (getRowChangeNum(move) == getColumnChangeNum(move)){
+            return true;
+        }
+        System.out.println("Error: Incorrect move logic for bishop");
+        return false;
+    }
+
+    private int getRowChangeNum(ChessMove move){
+        return Math.abs(move.getMoveFromRow() - move.getMoveToRow());
+    }
+
+    private int getColumnChangeNum(ChessMove move){
+        return Math.abs(move.getMoveFromColumn() - move.getMoveToColumn());
     }
 }
