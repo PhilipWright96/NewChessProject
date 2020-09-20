a88a73039aced8c1335327269489172d597c5f58
Philip Wright <wrightphilip4@gmail.com>
2020-09-20 14:37:23 +0100

    Have king move logic use move methods

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -13,24 +13,16 @@ public class King extends Piece {
     }
 
     private boolean rowChangeValid(ChessMove move){
-        if (getRowChangeNum(move) == 0 || getRowChangeNum(move) == 1){
+        if (move.getRowChangeNum() == 0 || move.getRowChangeNum() == 1){
             return true;
         }
         return false;
     }
 
     private boolean columnChangeValid(ChessMove move){
-        if (getColumnChangeNum(move) == 0 || getColumnChangeNum(move) == 1){
+        if (move.getColumnChangeNum() == 0 || move.getColumnChangeNum() == 1){
             return true;
         }
         return false;
-    }
-
-    private int getRowChangeNum(ChessMove move){
-        return Math.abs(move.getMoveFromRow() - move.getMoveToRow());
-    }
-
-    private int getColumnChangeNum(ChessMove move){
-        return Math.abs(move.getMoveFromColumn() - move.getMoveToColumn());
     }
 }
