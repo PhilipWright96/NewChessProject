4778a23f2634e137e41eadeb08eb346061aacfb8
Philip Wright <wrightphilip4@gmail.com>
2020-12-21 19:53:51 +0000

    Apply constructor changes to test

--- src/chess/pieces/BishopTest.java
+++ src/chess/pieces/BishopTest.java
@@ -3,6 +3,7 @@ package chess.pieces;
 import org.junit.Test;
 
 import chess.board.ChessBoard;
+import chess.board.ChessBoardGUI;
 import chess.game.ChessMove;
 import static org.junit.Assert.*;
 
@@ -10,11 +11,14 @@ public class BishopTest {
 
     @Test
     public void moveValid_withDiagonalMovePassed_returnsTrue(){
+
         // Given
         ChessMove diagonalMove = new ChessMove("a1-b2");
         Bishop bishop = new Bishop();
+
         // When
-        boolean result = bishop.moveValid(diagonalMove, new ChessBoard());
+        boolean result = bishop.moveValid(diagonalMove, new ChessBoard(new ChessBoardGUI()));
+        
         // Then
         assertEquals("Valid bishop move marked as invalid", true, result);
     }
@@ -24,8 +28,10 @@ public class BishopTest {
         // Given
         ChessMove diagonalMove = new ChessMove("a1-a2");
         Bishop bishop = new Bishop();
+
         // When
-        boolean result = bishop.moveValid(diagonalMove, new ChessBoard());
+        boolean result = bishop.moveValid(diagonalMove, new ChessBoard(new ChessBoardGUI()));
+
         // Then
         assertEquals("Invalid bishop move marked as valid", false, result);
     }
