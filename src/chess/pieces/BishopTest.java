e12e660ffddaa58e7b4eb98227d251229a6d8f06
Philip Wright <wrightphilip4@gmail.com>
2021-01-01 15:37:05 +0000

    Fix bishop tests

--- src/chess/pieces/BishopTest.java
+++ src/chess/pieces/BishopTest.java
@@ -3,34 +3,41 @@ package chess.pieces;
 import org.junit.Test;
 
 import chess.board.ChessBoard;
-import chess.board.ChessBoardGUI;
 import chess.game.ChessMove;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 
 public class BishopTest {
+    ChessBoard mockBoard = mock(ChessBoard.class);
+    ChessMove mockMove = mock(ChessMove.class);
 
     @Test
     public void moveValid_withDiagonalMovePassed_returnsTrue(){
 
         // Given
-        ChessMove diagonalMove = new ChessMove("a1-b2");
+        when(mockMove.getRowChangeNum()).thenReturn(1);
+        when(mockMove.getColumnChangeNum()).thenReturn(1);
+
         Bishop bishop = new Bishop();
 
         // When
-        boolean result = bishop.moveValid(diagonalMove, new ChessBoard(new ChessBoardGUI()));
+        boolean result = bishop.moveValid(mockMove, mockBoard);
         
         // Then
         assertEquals("Valid bishop move marked as invalid", true, result);
     }
 
     @Test
-    public void moveValid_withVerticalMovePassed_returnsFalse(){
+    public void moveValid_withNonDiagonalMovePassed_returnsFalse(){
         // Given
-        ChessMove diagonalMove = new ChessMove("a1-a2");
+        when(mockMove.getRowChangeNum()).thenReturn(1);
+        when(mockMove.getColumnChangeNum()).thenReturn(2);
+        
         Bishop bishop = new Bishop();
 
         // When
-        boolean result = bishop.moveValid(diagonalMove, new ChessBoard(new ChessBoardGUI()));
+        boolean result = bishop.moveValid(mockMove, mockBoard);
 
         // Then
         assertEquals("Invalid bishop move marked as valid", false, result);
