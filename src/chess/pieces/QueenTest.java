31a686e54089dcea8a76583002b2e35bd96c593d
Philip Wright <wrightphilip4@gmail.com>
2021-01-03 12:53:00 +0000

    Add unit tests for Queen

---
+++ src/chess/pieces/QueenTest.java
@@ -0,0 +1,62 @@
+package chess.pieces;
+
+import org.junit.Test;
+
+import chess.board.ChessBoard;
+import chess.game.ChessMove;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+
+public class QueenTest {
+    ChessBoard mockBoard = mock(ChessBoard.class);
+    ChessMove mockMove = mock(ChessMove.class);
+
+    @Test
+    public void moveValid_withValidDiagonalMove_returnsTrue(){
+
+        // Given
+        when(mockMove.getRowChangeNum()).thenReturn(2);
+        when(mockMove.getColumnChangeNum()).thenReturn(2);
+
+        Queen queen = new Queen();
+
+        // When
+        boolean result = queen.moveValid(mockMove, mockBoard);
+        
+        // Then
+        assertEquals("Valid queen move marked as invalid", true, result);
+    }
+
+    @Test
+    public void moveValid_withInvalidUnequalRowColumnMove_returnsFalse(){
+
+        // Given
+        when(mockMove.getRowChangeNum()).thenReturn(1);
+        when(mockMove.getColumnChangeNum()).thenReturn(2);
+
+        Queen queen = new Queen();
+
+        // When
+        boolean result = queen.moveValid(mockMove, mockBoard);
+        
+        // Then
+        assertEquals("Invalid queen move marked as valid", false, result);
+    }
+
+    @Test
+    public void moveValid_withValidHorizontalMove_returnsTrue(){
+
+        // Given
+        when(mockMove.getRowChangeNum()).thenReturn(5);
+        when(mockMove.getColumnChangeNum()).thenReturn(0);
+
+        Queen queen = new Queen();
+
+        // When
+        boolean result = queen.moveValid(mockMove, mockBoard);
+        
+        // Then
+        assertEquals("Valid queen move marked as invalid", true, result);
+    }
+}
