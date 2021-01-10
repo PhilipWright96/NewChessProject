6d7a10618c79e4393045e93a24cff6998e664323
Philip Wright <wrightphilip4@gmail.com>
2021-01-10 11:52:34 +0000

    Move updating methods to updater

--- src/chess/board/ChessBoardGUI.java
+++ src/chess/board/ChessBoardGUI.java
@@ -11,9 +11,11 @@ public class ChessBoardGUI implements IChessBoardGUI{
     private JButton[][] chessBoardGUIButtons = new JButton[8][8];
     private JPanel chessBoard = new JPanel(new GridLayout(0, 9));
     private GUISetter guiSetter;
+    private GUIUpdater guiUpdater;
 
-    public ChessBoardGUI(GUISetter guiSetter){
+    public ChessBoardGUI(GUISetter guiSetter, GUIUpdater guiUpdater){
         this.guiSetter = guiSetter;
+        this.guiUpdater = guiUpdater;
     }
 
     public void initializeBoardGUI(){
@@ -27,36 +29,14 @@ public class ChessBoardGUI implements IChessBoardGUI{
     }
 
     public void updateBoardWithPawns(){
-        for (int k = 0; k < chessBoardGUIButtons.length; k++){
-            JButton goldSquare = chessBoardGUIButtons[k][1];
-            ImageIcon goldIcon = new ImageIcon(ChessSprites.GOLD_PAWN);
-            goldSquare.setIcon(goldIcon);
-
-            JButton silverSquare = chessBoardGUIButtons[k][chessBoardGUIButtons.length - 2];
-            ImageIcon silverIcon = new ImageIcon(ChessSprites.SILVER_PAWN);
-            silverSquare.setIcon(silverIcon);
-        }
+        guiUpdater.updateBoardWithPawns(chessBoardGUIButtons);
     }
 
     public void updateBoardWithSpecialPieces(){
-        for (int l = 0, m = 0; l < chessBoardGUIButtons.length; l++, m++){
-            JButton goldSquare = chessBoardGUIButtons[l][0];
-            ImageIcon goldIcon = new ImageIcon(ChessSprites.SPRITES_IN_ORDER.get(l + m));
-            goldSquare.setIcon(goldIcon);
-
-            JButton silverSquare = chessBoardGUIButtons[l][chessBoardGUIButtons.length - 1];
-            ImageIcon silverIcon = new ImageIcon(ChessSprites.SPRITES_IN_ORDER.get(l + m + 1));
-            silverSquare.setIcon(silverIcon);
-        }
+        guiUpdater.updateBoardWithSpecialPieces(chessBoardGUIButtons);
     }
 
     public void updateBoardWithNewMove(ChessMove newMove, IPiece pieceBeingMoved){
-        System.out.println(" move from col is "  + newMove.getMoveFromColumn() + " move from row is " + newMove.getMoveFromRow());
-        JButton moveFromSquare = chessBoardGUIButtons[newMove.getMoveFromColumn()][newMove.getMoveFromRow()];
-        moveFromSquare.setIcon(null);
-
-        ImageIcon pieceImage = new ImageIcon(ChessSprites.getCorrespondingImageFromPiece(pieceBeingMoved));
-        JButton moveToSquare = chessBoardGUIButtons[newMove.getMoveToColumn()][newMove.getMoveToRow()];
-        moveToSquare.setIcon(pieceImage);
+        guiUpdater.updateBoardWithNewMove(chessBoardGUIButtons, newMove, pieceBeingMoved);
     }
 }
