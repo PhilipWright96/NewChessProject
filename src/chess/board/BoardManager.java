bbc5d021377be4540a735b89d568521885a21aca
Philip Wright <wrightphilip4@gmail.com>
2021-01-10 11:52:14 +0000

    Pass updater to board GUI

--- src/chess/board/BoardManager.java
+++ src/chess/board/BoardManager.java
@@ -2,7 +2,7 @@ package chess.board;
 
 public class BoardManager {
     public ChessBoard buildBoard(){
-        ChessBoardGUI boardGUI = new ChessBoardGUI(new GUISetter());
+        ChessBoardGUI boardGUI = new ChessBoardGUI(new GUISetter(), new GUIUpdater());
         return new ChessBoard(boardGUI);
     }
 }
