047cc294ddf5dad081a46aaeeba7761d51fecda4
Philip Wright <wrightphilip4@gmail.com>
2021-01-16 17:44:57 +0000

    Add GUI Tests

---
+++ src/chess/board/ChessBoardGUITest.java
@@ -0,0 +1,79 @@
+package chess.board;
+
+import org.junit.Test;
+
+import chess.game.ChessMove;
+import chess.pieces.IPiece;
+
+import static org.mockito.Mockito.*;
+
+import javax.swing.JPanel;
+
+
+public class ChessBoardGUITest {
+    @Test
+    public void initializeBoardGUI_callsCorrectSetterMethods(){
+
+        // Given
+        GUISetter mockGUISetter = mock(GUISetter.class);
+        GUIUpdater mockGUIUpdater = mock(GUIUpdater.class);
+        ChessBoardGUI boardGUI = new ChessBoardGUI(mockGUISetter, mockGUIUpdater);
+
+        // When
+        boardGUI.initializeBoardGUI();
+
+        // Then
+        verify(mockGUISetter).arrangeGUIScreen(any(JPanel.class), any(JPanel.class));
+        verify(mockGUISetter).arrangeButtons(any());
+        verify(mockGUISetter).addButtonsToBoard(any(JPanel.class), any());
+        verify(mockGUISetter).addFrameToBoard(any(JPanel.class));
+    }
+
+    @Test
+    public void updateBoardWithPawns_callsCorrectUpdaterMethod(){
+
+        // Given
+        GUISetter mockGUISetter = mock(GUISetter.class);
+        GUIUpdater mockGUIUpdater = mock(GUIUpdater.class);
+        ChessBoardGUI boardGUI = new ChessBoardGUI(mockGUISetter, mockGUIUpdater);
+
+        // When
+        boardGUI.updateBoardWithPawns();
+
+        // Then
+        verify(mockGUIUpdater).updateBoardWithPawns(any());
+    }
+
+    @Test
+    public void updateBoardWithSpecialPieces_callsCorrectUpdaterMethod(){
+
+        // Given
+        GUISetter mockGUISetter = mock(GUISetter.class);
+        GUIUpdater mockGUIUpdater = mock(GUIUpdater.class);
+        ChessBoardGUI boardGUI = new ChessBoardGUI(mockGUISetter, mockGUIUpdater);
+
+        // When
+        boardGUI.updateBoardWithSpecialPieces();
+
+        // Then
+        verify(mockGUIUpdater).updateBoardWithSpecialPieces(any());
+    }
+
+    @Test
+    public void updateBoardWithNewMove_callsCorrectUpdaterMethod(){
+
+        // Given
+        GUISetter mockGUISetter = mock(GUISetter.class);
+        GUIUpdater mockGUIUpdater = mock(GUIUpdater.class);
+        ChessBoardGUI boardGUI = new ChessBoardGUI(mockGUISetter, mockGUIUpdater);
+
+        ChessMove mockMove = mock(ChessMove.class);
+        IPiece mockPiece = mock(IPiece.class);
+
+        // When
+        boardGUI.updateBoardWithNewMove(mockMove, mockPiece);
+
+        // Then
+        verify(mockGUIUpdater).updateBoardWithNewMove(any(), eq(mockMove), eq(mockPiece));
+    }
+}
