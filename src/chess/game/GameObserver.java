203c6ed857bfb5ec111bb0004d2e3869c27b0cab
Philip Wright <wrightphilip4@gmail.com>
2021-03-13 16:48:20 +0000

    Remove now unused logic from observer

--- src/chess/game/GameObserver.java
+++ src/chess/game/GameObserver.java
@@ -1,20 +1,9 @@
 package chess.game;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import chess.board.ChessBoard;
-import chess.board.IChessBoard;
-import chess.input.ClearPathChecker;
-import chess.pieces.PieceFactory;
-import chess.pieces.Piece.Types;
-import chess.player.Player;
-import chess.util.Teams;
-import chess.pieces.IPiece;
-
 public class GameObserver {
     IChessGame game;
     boolean hasFinished = false;
+    
     public GameObserver(IChessGame game){
         this.game = game;
         this.game.attach(this);
@@ -28,46 +17,5 @@ public class GameObserver {
         return hasFinished;
     }
 
-    public boolean opposingKingInCheck(Player playerMoving, IChessBoard board){
-        ChessBoard.PieceToCoordinates pieceToCoordinatesMap = board.getPieceToCoordinatesMap();
-        ChessBoard.Coordinates opposingKingCoordinates;
-        HashMap<IPiece, ChessBoard.Coordinates> movingPlayerPiecesToCoords;
-
-        ClearPathChecker pathChecker = new ClearPathChecker();
-
-        if (playerMoving.getTeam() == Teams.SILVER){
-            opposingKingCoordinates = pieceToCoordinatesMap.goldPieceToCoordinates.get(PieceFactory.constructPiece(Types.KING, Teams.GOLD));
-            movingPlayerPiecesToCoords = pieceToCoordinatesMap.silverPieceToCoordinates;
-        }
-        else {
-            opposingKingCoordinates = pieceToCoordinatesMap.silverPieceToCoordinates.get(PieceFactory.constructPiece(Types.KING, Teams.SILVER));
-            movingPlayerPiecesToCoords = pieceToCoordinatesMap.goldPieceToCoordinates;
-        }
-
-        for (Map.Entry<IPiece, ChessBoard.Coordinates> entry: movingPlayerPiecesToCoords.entrySet()){
-            IPiece piece = entry.getKey();
-            ChessBoard.Coordinates coordinates = entry.getValue();
-            boolean potentialMoveValid = false;
-            boolean pathForPotentialMoveClear = false;
-
-            ChessMove potentialMove = new ChessMove(coordinates.getColumnCoordinate(), coordinates.getRowCoordinate(), opposingKingCoordinates.getColumnCoordinate(), opposingKingCoordinates.getRowCoordinate());
-
-            potentialMoveValid = piece.moveValid(potentialMove, board);
-
-            if (potentialMoveValid){
-                if (piece.getType() == Types.KNIGHT){
-                    pathForPotentialMoveClear = true;
-                }
-                else {
-                    pathForPotentialMoveClear = pathChecker.pathForMoveClear(potentialMove, board.getPieceArray());
-                }
-            }
-            
-            if (potentialMoveValid && pathForPotentialMoveClear){
-                return true;
-            }
-        }
-
-        return false;
-    }
+    
 }
