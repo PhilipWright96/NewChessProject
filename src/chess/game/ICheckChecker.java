ca9f789f6a1c3e04b7ccb40a8fb9d8575d595d3f
Philip Wright <wrightphilip4@gmail.com>
2021-03-14 14:29:20 +0000

    Update interfaces

--- src/chess/game/ICheckChecker.java
+++ src/chess/game/ICheckChecker.java
@@ -1,9 +1,10 @@
 package chess.game;
 
 import chess.board.IChessBoard;
+import chess.input.ClearPathChecker;
 import chess.player.Player;
 
 public interface ICheckChecker {
-    public boolean ownKingInCheck(ChessMove move, Player playerMoving, IChessBoard board);
-    public boolean opposingKingInCheck(Player playerMoving, IChessBoard board);
+    public boolean ownKingInCheck(ChessMove move, Player playerMoving, IChessBoard board, ClearPathChecker pathChecker);
+    public boolean opposingKingInCheck(Player playerMoving, IChessBoard board, ClearPathChecker pathChecker);
 }
