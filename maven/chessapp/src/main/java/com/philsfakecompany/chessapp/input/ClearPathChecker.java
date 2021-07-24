95485da8df927fb83193b870d957c8e6fb4867db
Philip Wright <wrightphilip4@gmail.com>
2021-07-24 20:42:37 +0100

    Add coordinate retrieving methods

--- maven/chessapp/src/main/java/com/philsfakecompany/chessapp/input/ClearPathChecker.java
+++ maven/chessapp/src/main/java/com/philsfakecompany/chessapp/input/ClearPathChecker.java
@@ -1,6 +1,7 @@
 package input;
 
 import game.*;
+import java.util.ArrayList;
 import pieces.*;
 
 public class ClearPathChecker {
@@ -192,5 +193,155 @@ public class ClearPathChecker {
             }
         }
         return false;
+    }
+
+    public ArrayList<Coordinates> getCoordinatesBetweenMove(
+        ChessMove move,
+        boolean checkMoveTargetSpace
+    ) {
+        if (move.isStraight()) {
+            if (move.isHorizontal()) {
+                return getCoordinatesForHorizontalMove(
+                    move,
+                    checkMoveTargetSpace
+                );
+            } else {
+                return getCoordinatesForVerticalMove(
+                    move,
+                    checkMoveTargetSpace
+                );
+            }
+        } else {
+            return getCoordinatesForDiagonalMove(move, checkMoveTargetSpace);
+        }
+    }
+
+    private ArrayList<Coordinates> getCoordinatesForHorizontalMove(
+        ChessMove move,
+        boolean checkMoveTargetSpace
+    ) {
+        ArrayList<Coordinates> coordinatesBetweenMove = new ArrayList<Coordinates>();
+        int moveFromRow = move.getMoveFromRow();
+        int moveFromCol = move.getMoveFromColumn();
+
+        int moveToCol = move.getMoveToColumn();
+        int augmentVal = checkMoveTargetSpace == true ? 1 : 0;
+        if (moveFromCol < moveToCol) {
+            for (
+                int col = moveFromCol + 0;
+                col < moveToCol + augmentVal;
+                col++
+            ) {
+                coordinatesBetweenMove.add(new Coordinates(moveFromRow, col));
+            }
+        }
+        // Else moving left
+        else {
+            for (
+                int col = moveFromCol - 0;
+                col > moveToCol - augmentVal;
+                col--
+            ) {
+                coordinatesBetweenMove.add(new Coordinates(moveFromRow, col));
+            }
+        }
+        return coordinatesBetweenMove;
+    }
+
+    private ArrayList<Coordinates> getCoordinatesForVerticalMove(
+        ChessMove move,
+        boolean checkMoveTargetSpace
+    ) {
+        ArrayList<Coordinates> coordinatesBetweenMove = new ArrayList<Coordinates>();
+        int moveFromRow = move.getMoveFromRow();
+        int moveFromCol = move.getMoveFromColumn();
+
+        int moveToRow = move.getMoveToRow();
+
+        int augmentVal = checkMoveTargetSpace == true ? 1 : 0;
+
+        // If moving down
+        if (moveFromRow < moveToRow) {
+            for (
+                int row = moveFromRow + 1;
+                row < moveToRow + augmentVal;
+                row++
+            ) {
+                coordinatesBetweenMove.add(new Coordinates(row, moveFromCol));
+            }
+        }
+        // Else moving up
+        else {
+            for (
+                int row = moveFromRow - 1;
+                row > moveToRow - augmentVal;
+                row--
+            ) {
+                coordinatesBetweenMove.add(new Coordinates(row, moveFromCol));
+            }
+        }
+        return coordinatesBetweenMove;
+    }
+
+    private ArrayList<Coordinates> getCoordinatesForDiagonalMove(
+        ChessMove move,
+        boolean checkMoveTargetSpace
+    ) {
+        ArrayList<Coordinates> coordinatesBetweenMove = new ArrayList<Coordinates>();
+        int moveFromRow = move.getMoveFromRow();
+        int moveFromCol = move.getMoveFromColumn();
+
+        int moveToRow = move.getMoveToRow();
+        int moveToCol = move.getMoveToColumn();
+
+        int augmentVal = checkMoveTargetSpace == true ? 1 : 0;
+
+        // If moving up
+        if (moveFromRow > moveToRow) {
+            // If moving right
+            if (moveFromCol < moveToCol) {
+                for (
+                    int row = moveFromRow - 1, col = moveFromCol + 1;
+                    row >= moveToRow - augmentVal;
+                    row--, col++
+                ) {
+                    coordinatesBetweenMove.add(new Coordinates(row, col));
+                }
+            }
+            // Else moving left
+            else {
+                for (
+                    int row = moveFromRow - 1, col = moveFromCol - 1;
+                    row >= moveToRow - augmentVal;
+                    row--, col--
+                ) {
+                    coordinatesBetweenMove.add(new Coordinates(row, col));
+                }
+            }
+        }
+        // Else moving down
+        else {
+            // If moving right
+            if (moveFromCol < moveToCol) {
+                for (
+                    int row = moveFromRow + 1, col = moveFromCol + 1;
+                    row <= moveToRow + augmentVal;
+                    row++, col++
+                ) {
+                    coordinatesBetweenMove.add(new Coordinates(row, col));
+                }
+            }
+            // Else moving left
+            else {
+                for (
+                    int row = moveFromRow + 1, col = moveFromCol - 1;
+                    row <= moveToRow + augmentVal;
+                    row++, col--
+                ) {
+                    coordinatesBetweenMove.add(new Coordinates(row, col));
+                }
+            }
+        }
+        return coordinatesBetweenMove;
     }
 }
