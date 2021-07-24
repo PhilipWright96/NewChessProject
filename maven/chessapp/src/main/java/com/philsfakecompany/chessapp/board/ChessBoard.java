9712feb09c7381fad14439fa5e2b10106dedb8b1
Philip Wright <wrightphilip4@gmail.com>
2021-07-24 15:38:51 +0100

    Move coordinates to its own class

--- maven/chessapp/src/main/java/com/philsfakecompany/chessapp/board/ChessBoard.java
+++ maven/chessapp/src/main/java/com/philsfakecompany/chessapp/board/ChessBoard.java
@@ -176,31 +176,4 @@ public class ChessBoard implements IChessBoard {
             }
         }
     }
-
-    public class Coordinates {
-
-        private int rowCoordinate;
-        private int columnCoordinate;
-
-        public Coordinates(int rowCoordinate, int columnCoordinate) {
-            this.rowCoordinate = rowCoordinate;
-            this.columnCoordinate = columnCoordinate;
-        }
-
-        public int getRowCoordinate() {
-            return this.rowCoordinate;
-        }
-
-        public int getColumnCoordinate() {
-            return this.columnCoordinate;
-        }
-
-        private void setRowAndColumnCoordinates(
-            int rowCoordinate,
-            int columnCoordinate
-        ) {
-            this.rowCoordinate = rowCoordinate;
-            this.columnCoordinate = columnCoordinate;
-        }
-    }
 }
